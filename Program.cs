750b7f2544b128ec9c601ec0d38fd680cd46e0bc
Pedro Alves <pedro22carro@gmail.com>
2020-12-07 22:53:13 -0300

    Validando CPF e CNPJ

--- Program.cs
+++ Program.cs
@@ -9,19 +9,53 @@ namespace AulaPOO_Heranca
         {
             // Instanciamos nossa subclasse
             PessoaFisica pf = new PessoaFisica();
-            PessoaFisica pessoaFisica = new PessoaFisica();
+            PessoaJuridica pj = new PessoaJuridica();
 
             // Atribuímos um valor para a propriedade disponível na superclasse
-            pf.nome = "Pedro";
+            Console.WriteLine("Digite seu nome");
+            pf.nome = Console.ReadLine();
 
-            Console.WriteLine("Digite seu CPF");
-            pf.cpf = Console.ReadLine();
-            
             // Mostramos no console o método de saudação da superclasse
             Console.WriteLine( pf.DarBoasVindas(pf.nome) );
+            bool opcao = true;
+            do
+            {
+                Console.WriteLine("Você é uma pessoa física ou jurídica [f]-física [j]-jurídica");
+                string opcao2 = Console.ReadLine();
+                switch (opcao2)
+                {
+                    case "f":
+                        Console.WriteLine("Digite seu CPF");
+                        pf.cpf = Console.ReadLine();
+                    
+                        // Mostramos no console o método de validação da subclasse
+                        bool cpf = pf.ValidarCPF(pf.cpf);
+                        if (cpf == true)
+                        {
+                            Console.WriteLine("O seu CPF é válido");
+                        }else{
+                            Console.WriteLine("O seu CPF é inválido");  
+                        }
+                        opcao = false;
+                        break;
+                    case "j":
+                        Console.WriteLine("Digite seu CNPJ");
+                        pj.cnpj = Console.ReadLine();
 
-            // Mostramos no console o método de validação da subclasse
-            Console.WriteLine( pf.ValidarCPF(pf.cpf) );
+                        bool cnpj = pj.ValidarCNPJ(pj.cnpj);
+                        if (cnpj == true)
+                        {
+                            Console.WriteLine("O seu CNPJ é válido");
+                        }else{
+                            Console.WriteLine("O seu CNPJ é inválido");  
+                        }
+                        opcao = false;
+                        break;
+                    default:
+                        Console.WriteLine("Você digitou uma opção inválida");
+                        break;
+                }
+            } while (opcao != false);
         }
     }
 }
